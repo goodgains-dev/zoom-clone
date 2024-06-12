57fd8151f45fc84fd5a2e53408431d67727a81e8
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-06-11 22:06:07 -0400

    Update page.tsx
    1

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   const now = new Date();
 
   const time = now.toLocaleTimeString('en-US', { timeZone: 'America/New_York', hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  const date = (new Intl.DateTimeFormat('en-US', { timeZone: 'America/New_York', dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
