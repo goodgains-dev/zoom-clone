b9c4aa3a21a86d2abd6f61bd12369cd16982edef
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-06-28 11:47:05 -0400

    Update page.tsx
    1

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { useUser } from '@clerk/nextjs';
 import { StreamChat, Channel, ChannelFilters, ChannelSort, DefaultGenerics } from 'stream-chat';
