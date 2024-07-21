f765195ae40dfbf51aab017c1dfd7e3141b52c4c
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-07-21 16:29:32 -0400

    Update Navbar.tsx
    1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,32 +1,42 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
-import { SignedIn, UserButton } from '@clerk/nextjs';
-
+import { OrganizationSwitcher, SignedIn, UserButton } from "@clerk/nextjs";
 import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-      <Link href="/" className="flex items-center gap-1">
-        <Image
-          src="/icons/logo.svg"
-          width={32}
-          height={32}
-          alt="yoom logo"
-          className="max-sm:size-10"
-        />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          GoodGains Work
-        </p>
-      </Link>
-      <div className="flex-between gap-5">
-        <SignedIn>
-          <UserButton afterSignOutUrl="/sign-in" />
-        </SignedIn>
-
-        <MobileNav />
-      </div>
-    </nav>
+    <>
+      <style jsx global>{`
+        .cl-organization-switcher, .cl-organization-switcher * {
+          color: white !important;
+        }
+      `}</style>
+      <nav className="flex-between fixed z-50 w-full bg-white-1 px-6 py-4 lg:px-10">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={32}
+            height={32}
+            alt="yoom logo"
+            className="max-sm:size-10"
+          />
+          <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+            GoodGains Work
+          </p>
+        </Link>
+        <div className="flex-between gap-5">
+          <SignedIn>
+            <div className="flex items-center gap-5">
+              <OrganizationSwitcher />
+              <UserButton afterSignOutUrl="/sign-in" />
+            </div>
+          </SignedIn>
+          <MobileNav />
+        </div>
+      </nav>
+    </>
   );
 };
 
