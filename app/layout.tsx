5055443ae3a8f3b3032f1fa0582b15805598b69d
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-08-13 00:46:31 -0400

    Update layout.tsx
    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,61 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
+import { SettingsProvider } from '@/components/SettingsProvider'; 
+import { Toaster } from "@/components/ui/toaster";
+import Provider from "./Provider";
 
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+
+// Load fonts
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
-  title: "YOOM",
-  description: "Video calling App",
+  title: "GoodGains Work",
+  description: "Social Productivity App",
   icons: {
     icon: "/icons/logo.svg",
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <ClerkProvider
-        appearance={{
-          layout: {
-            socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
-          },
-          variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#1C1F2E",
-            colorInputBackground: "#252A41",
-            colorInputText: "#fff",
-          },
-        }}
-      >
-        <body className={`${inter.className} bg-dark-2`}>
-          <Toaster />
-          {children}
+    <ClerkProvider
+      appearance={{
+        baseTheme: "dark",
+        layout: {
+          socialButtonsVariant: "iconButton",
+          logoImageUrl: "/icons/yoom-logo.svg",
+        },
+        variables: { 
+          colorPrimary: "#3371FF",
+          colorText: "#fff",
+          colorBackground: "#1C1F2E",
+          colorInputBackground: "#252A41",
+          colorInputText: "#fff",
+          fontSize: "16px",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`min-h-screen font-sans antialiased ${fontSans.variable} bg-dark-2`}
+        >
+          <SettingsProvider>
+            <Provider>
+              <Toaster />
+              {children}
+            </Provider>
+          </SettingsProvider>
         </body>
-      </ClerkProvider>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
